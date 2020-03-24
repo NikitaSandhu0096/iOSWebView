42a37cac631a9aa91cf1b0a92ad3861e67624c85
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-24 19:45:47 -0400

    web view with history

--- iOSWebView/ViewController.swift
+++ iOSWebView/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myWebKitView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadLambtonUrl()
+    }
+    
+    @IBAction func btnHistryClick(_ sender: UIBarButtonItem) {
+        if myWebKitView.canGoBack{
+            let historyList = myWebKitView.backForwardList.backList
+            if historyList.count > 0{
+                for item in historyList {
+                    print("\(String(describing: item.title!)) - \(item.url.absoluteURL)")
+                }
+            }
+        }
+    }
+    
+    func loadLambtonUrl(){
+        let url = URL(string: "https://www.lambtoncollege.ca")
+        let urlReq = URLRequest(url: url!)
+        myWebKitView.load(urlReq)
     }
 
-
+    func loadHtmlString(){
+        let htmlString = "<h1>Hello World</h1>"
+        myWebKitView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    
+    @IBAction func btnNavigation(_ sender: UIBarButtonItem) {
+        switch sender.tag {
+        case 0: // Home Button is press
+            loadLambtonUrl()
+//            myWebKitView.load(URLRequest(url: URL(string: "https://www.lambtoncollege.ca")!))
+        case 1: // Prev Button is pressed
+            if myWebKitView.canGoBack{
+                myWebKitView.goBack()
+            }
+        case 2:
+            if myWebKitView.canGoForward{
+                myWebKitView.goForward()
+            }
+        case 3:
+            myWebKitView.reloadFromOrigin()
+        
+        default:
+            print("No Navigation action found")
+        }
+    }
 }
 
